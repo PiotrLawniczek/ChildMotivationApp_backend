fbc44e366cba3e54f4fb6792be12f46a99056f20
Piotr Ławniczek <piotr.lawniczek93@gmail.com>
2017-12-13 20:37:36 +0100

    JPA Repositories

--- src/main/java/pl/lodz/childmotivationapp/repository/ProgressRepository.java
+++ src/main/java/pl/lodz/childmotivationapp/repository/ProgressRepository.java
@@ -3,16 +3,14 @@ package pl.lodz.childmotivationapp.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
-import pl.lodz.childmotivationapp.entity.User;
+import pl.lodz.childmotivationapp.entity.Progress;
 
 /**
- * Created by elawpio on 2017-12-11.
+ * Created by elawpio on 2017-12-13.
  */
 @Transactional
 @Repository
-public interface UserRepository  extends JpaRepository<User, Long> {
-    User findById(long id);
-
-
+public interface ProgressRepository extends JpaRepository<Progress, Long> {
+    Progress findById(long id);
 
 }
